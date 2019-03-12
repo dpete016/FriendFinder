6cd6d9e13fc58e135e92cbdb7e1f0e26df0fb1c0
Dpete016 <dpete016@gmail.com>
2019-03-12 14:51:19 -0400

    progress

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [
+
+    {
+        name:"Chuck",
+        photo:"",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    }
+
+    
+];
+
+module.exports = friends;
