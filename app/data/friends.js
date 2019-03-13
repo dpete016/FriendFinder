1f6b3e25dd4e216ee90c6412ccfe18af3c0b2ead
Dan <dpete016@gmail.com>
2019-03-12 21:21:36 -0400

    in progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,95 @@ var friends = [
 
     {
         name:"Chuck",
-        photo:"",
+        photo:"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "3",
+            "1",
+            "4",
+            "3",
+            "5",
+            "1",
+            "2",
+            "2",
+            "4",
+            "1"
+        ]
+    }
+  
+    {
+        name:"Josiah",
+        photo:"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    }
+    
+
+    {
+        name:"Manndy",
+        photo:"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "5",
+            "1",
+            "2",
+            "4",
+            "5",
+            "3",
+            "2",
+            "5",
+            "4",
+            "5"
+        ]
+    }
+
+
+    {
+        name:"Homer",
+        photo:"https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "1",
+            "1",
+            "3",
+            "4",
+            "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "1"
+        ]
+    }
+
+
+    {
+        name:"Terry",
+        photo:"https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "3",
+            "1",
+            "2",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    }
+
+    {
+        name:"Dan",
+        photo:"https://images.pexels.com/photos/936593/pexels-photo-936593.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         scores:[
             "5",
             "1",
@@ -17,7 +105,74 @@ var friends = [
         ]
     }
 
-    
+    {
+        name:"Jerome",
+        photo:"https://images.pexels.com/photos/793253/pexels-photo-793253.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "1",
+            "1",
+            "2",
+            "3",
+            "3",
+            "1",
+            "2",
+            "3",
+            "5",
+            "4"
+        ]
+    }
+
+    {
+        name:"Favaro",
+        photo:"https://images.pexels.com/photos/21278/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "3",
+            "1",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "5",
+            "3",
+            "1"
+        ]
+    }
+
+    {
+        name:"Derrick",
+        photo:"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "1",
+            "2",
+            "2",
+            "4",
+            "5",
+            "3",
+            "5",
+            "3",
+            "4",
+            "5"
+        ]
+    }
+
+    {
+        name:"Rock",
+        photo:"https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        scores:[
+            "1",
+            "1",
+            "4",
+            "5",
+            "5",
+            "5",
+            "2",
+            "2",
+            "2",
+            "3"
+        ]
+    }
+
 ];
 
 module.exports = friends;
